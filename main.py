d09b18ecff493b084b7144bbb90d7b478ece499a
jinseisieko <jinseisieko@gmail.com>
2024-12-08 16:28:08 +0300

    pygame window

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+import math
 from pprint import pprint
 
 import numpy as np
+import pygame
 
 # CONSTS
 
 # INPUT
 room_size_x, room_size_y = tuple(map(float, input("Room size:").split()))
+K = 400 / room_size_y
 print("---Characteristics of the robot vacuum cleaner---")
 radius = float(input("Radius:"))
-K = float(input("The distance between the wheels:")) / 2
+dist_wheels = float(input("The distance between the wheels:")) / 2
 loc_angle = float(input("The laser locator angle (radian):"))
 x, y = tuple(map(float, input("The start position of robot:").split()))
+alpha = 0
 size_cell = 3 * radius
 rows = int(room_size_x // size_cell + 1)
 cols = int(room_size_y // size_cell + 1)
@@ -29,9 +33,41 @@ obstacles = np.zeros((N_segments, 2, 2), dtype=float)  # processed data
 for i, obs in enumerate(obst_):
     for j, point in enumerate(obs):
         obstacles[i + j, :, :] = np.array(
-            [[point[0], point[1]],
-             [obs[(j + 1) % len(obs)][0], obs[(j + 1) % len(obs)][1]]]
+            [
+                [point[0], point[1]],
+                [obs[(j + 1) % len(obs)][0], obs[(j + 1) % len(obs)][1]],
+            ]
         )
-pprint(obstacles)
-# *BRAIN* OF ROBOT
-data = np.zeros((rows, cols, 10000, 3), dtype=float)
+
+data = np.zeros((rows, cols, 1000, 3), dtype=float)  # *BRAIN* OF ROBOT
+
+def simulation(time):
+    ...
+
+pygame.init()
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((2 * K * room_size_x, 2 * K * room_size_y))
+pygame.display.set_caption("moving the robot vacuum-cleaner in the room")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+    simulation(1 / clock.get_fps() if clock.get_fps() != 0 else 0)
+    main_surface = pygame.surface.Surface((K * room_size_x, K * room_size_y))
+    main_surface.fill("white")
+    pygame.draw.circle(main_surface, "black", (K * x, K * y), radius * K, 1)
+    pygame.draw.line(
+        main_surface,
+        "black",
+        (K * x, K * y),
+        np.array([K * x, K * y]) + np.array([math.cos(alpha), math.sin(alpha)]) * K * radius,
+    )
+    for i in range(N_segments):
+        pygame.draw.line(main_surface, "black", obstacles[i, 0, :]*K, obstacles[i, 1, :]*K)
+
+    screen.blit(main_surface, (0, 0))
+    pygame.display.update()
+
+    clock.tick(120)
